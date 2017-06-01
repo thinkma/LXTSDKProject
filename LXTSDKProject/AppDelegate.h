55373e5805c543593f716a7848bf5a2ed7ade95d
apple <think_am@126.com>
2017-06-01 11:23:10 +0800

    g

---
+++ LXTSDKProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LXTSDKProject
+//
+//  Created by apple on 5/17/17.
+//  Copyright © 2017 mawei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LXTSDKProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LXTSDKProject
+//
+//  Created by apple on 5/17/17.
+//  Copyright © 2017 mawei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
